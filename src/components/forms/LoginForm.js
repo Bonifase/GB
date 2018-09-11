d351e375c28fed3cefe75ae3fbce95ed070c55f3
Bonifase Orwa <orwabonifase@gmail.com>
2018-09-11 10:42:49 +0300

    add login form

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LoginForm extends React.component {
+    state = {}
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default LoginForm;
