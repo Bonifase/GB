5e5e042ddaba3f18b01cb44b44e129bc71671c41
Bonifase Orwa <orwabonifase@gmail.com>
2018-09-16 20:16:47 +0300

    add user routes

--- src/components/routes/UserRoute.js
+++ src/components/routes/UserRoute.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
 
 
-const UserRoute = ({ component: component, ...rest}) => (
-    <Route {...rest} render={props => <component {...props} />} />
+const UserRoute = ({ isAuthenticated, component: component, ...rest}) => (
+    <Route {...rest} render={props => isAuthenticated ? <component {...props} /> : <Redirect to='/login' />} />
 );
 
 UserRoute.propTypes = {
-    component: PropTypes.func.isRequired
+    component: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool.isRequired
 };
+function mapStateToProps(state) {
+    return {
+        isAuthenticated: !!state.user.token
+    }
+}
 
-export default UserRoute;
+export default connect(mapStateToProps)(UserRoute);
