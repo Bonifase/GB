65f098f3fc1c65184c3f7bcb291c499b2bb125d9
Bonifase Orwa <orwabonifase@gmail.com>
2018-09-19 21:06:21 +0300

    add validate reset token action

--- src/api.js
+++ src/api.js
@@ -7,6 +7,9 @@ export default {
         confirm: token => axios.post('https://web-game-board-api.herokuapp.com/api/auth/confirmation', { token })
         .then(res => res.data.user),
         resetPasswordRequest: email =>
-        axios.post("https://web-game-board-api.herokuapp.com/api/auth/reset_password_request", { email })
+        axios.post("https://web-game-board-api.herokuapp.com/api/auth/reset_password_request", { email }), 
+        validateToken: token => axios.post("/api/auth/validate_token", { token }),
+        resetPassword: data => axios.post("/api/auth/reset_password", { data })
+  
     }
 };   
