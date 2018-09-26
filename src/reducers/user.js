2a5a39939936eb608b1a14bfa1bfbfff7417be21
Bonifase Orwa <orwabonifase@gmail.com>
2018-09-26 07:00:32 +0300

    set loaded to false

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,6 +1,6 @@
 import { USER_LOGGED_IN, USER_LOGGED_OUT, USER_FETCHED  } from "../types";
 
-export default function user(state = {loaded: false}, action = {}) {
+export default function user(state = { loaded: false }, action = {}) {
     switch(action.type) {
         case USER_LOGGED_IN:
             return action.user;
