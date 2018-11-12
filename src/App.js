5e8fb822fa64e3d6595d03ef48392514f6392cf7
Bonifase <orwabonifase@gmail.com>
2018-11-12 20:32:29 +0300

    fetch current user

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { connect } from "react-redux";
 import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Loader from 'react-loader';
+<<<<<<< HEAD
+=======
+import { connect } from "react-redux";
+>>>>>>> fetch current user
 import 'semantic-ui-css/semantic.min.css';
 import "./index.css"
 import HomePage from './components/pages/HomePage';
@@ -13,12 +17,17 @@ import ForgotPasswordPage from './components/pages/ForgotPasswordPage';
 import ResetPasswordPage from './components/pages/ResetPasswordPage';
 import LoginPage from './components/pages/LoginPage';
 import DashboardPage from './components/pages/DashboardPage';
-import GamesPage from './components/pages/GamesPage';
+import TopNavigation from './components/navigation/TopNavigation';
 import UserRoute from './components/routes/UserRoute';
 import GuestRoute from './components/routes/GuestRoute';
+<<<<<<< HEAD
 import { fetchCurrentUser } from "./actions/users";
 
 
+=======
+import { fetchCurrentUser } from './actions/users';
+
+>>>>>>> fetch current user
 class App extends React.Component {
     componentDidMount() {
       if (this.props.isAuthenticated) this.props.fetchCurrentUser();
@@ -27,6 +36,7 @@ class App extends React.Component {
     render() {
       const { location, isAuthenticated, loaded } = this.props;
       return (
+<<<<<<< HEAD
             <div className="ui container">
             <Loader loaded={loaded}>
             { isAuthenticated && <TopNavigation/> }
@@ -62,3 +72,68 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps, { fetchCurrentUser })(App);
+=======
+          <div>
+            <Loader loaded={loaded}>
+              {isAuthenticated && <TopNavigation />}
+              <Route location={location} path="/" exact component={HomePage} />
+              <Route
+                location={location}
+                path="/confirmation/:token"
+                exact
+                component={ConfirmationPage}
+              />
+              <GuestRoute
+                location={location}
+                path="/login"
+                exact
+                component={LoginPage}
+              />
+              <GuestRoute
+                location={location}
+                path="/signup"
+                exact
+                component={SignupPage}
+              />
+              <GuestRoute
+                location={location}
+                path="/forgot_password"
+                exact
+                component={ForgotPasswordPage}
+              />
+              <GuestRoute
+                location={location}
+                path="/reset_password/:token"
+                exact
+                component={ResetPasswordPage}
+              />
+              <UserRoute
+                location={location}
+                path="/dashboard"
+                exact
+                component={DashboardPage}
+              />
+            </Loader>
+          </div>
+      );
+    }
+  }
+  
+  App.propTypes = {
+    location: PropTypes.shape({
+      pathname: PropTypes.string.isRequired
+    }).isRequired,
+    isAuthenticated: PropTypes.bool.isRequired,
+    fetchCurrentUser: PropTypes.func.isRequired,
+    loaded: PropTypes.bool.isRequired
+  };
+  
+  function mapStateToProps(state) {
+    return {
+      isAuthenticated: !!state.user.email,
+      loaded: state.user.loaded
+    };
+  }
+  
+  export default connect(mapStateToProps, { fetchCurrentUser })(App);
+>>>>>>> fetch current user
--- src/actions/users.js
+++ src/actions/users.js
@@ -1,13 +1,25 @@
+<<<<<<< HEAD
 import { USER_FETCHED } from '../types';
+=======
+import {USER_FETCHED} from '../types'
+>>>>>>> fetch current user
 import { userLoggedIn } from './auth';
 import api from '../api';
 
 export const userFetched = user => ({
     type: USER_FETCHED,
     user
+<<<<<<< HEAD
     });
+=======
+})
+>>>>>>> fetch current user
 
 export const signup = (data) => dispatch => api.user.signup (data).then(user =>
     dispatch(userLoggedIn(user)));
 
+<<<<<<< HEAD
 export const fetchCurrentUser = () => (dispatch) => api.user.fetchCurrentUser().then(user => dispatch(userFetched(user)));
+=======
+export const fetchCurrentUser = () => (dispatch) => api.user.fetchCurrentUser().then(user => dispatch(userFetched(user )));
+>>>>>>> fetch current user
